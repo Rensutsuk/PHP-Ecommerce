8b1e92d43d00c6bd013d532a729e3e50895b8f73
Rensutsuk <96723635+Rensutsuk@users.noreply.github.com>
2023-04-26 02:21:04 +0800

    commited

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
+<?<php>
+<html>
+<body>
 
-echo "Hello World!";
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+</php>
