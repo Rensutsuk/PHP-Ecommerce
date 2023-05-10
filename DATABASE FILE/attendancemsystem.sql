69f47d9d0a5d1b5849950fab162e70702ae10c88
Rensutsuk <johnrenzoerfelp@gmail.com>
2023-05-10 18:15:28 +0800

    commited

--- DATABASE FILE/attendancemsystem.sql
+++ DATABASE FILE/attendancemsystem.sql
@@ -7,8 +7,10 @@
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,19 +28,28 @@ SET time_zone = "+00:00";
 -- Table structure for table `tbladmin`
 --
 
-CREATE TABLE `tbladmin` (
-  `Id` int(10) NOT NULL,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `emailAddress` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+CREATE TABLE `tbladmin`
+(
+  `Id` int
+(10) NOT NULL,
+  `firstName` varchar
+(50) NOT NULL,
+  `lastName` varchar
+(50) NOT NULL,
+  `emailAddress` varchar
+(50) NOT NULL,
+  `password` varchar
+(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbladmin`
 --
 
-INSERT INTO `tbladmin` (`Id`, `firstName`, `lastName`, `emailAddress`, `password`) VALUES
+INSERT INTO `tbladmin` (`
+Id`,
+`firstName
+`, `lastName`, `emailAddress`, `password`) VALUES
 (1, 'Admin', 'Liam', 'admin@mail.com', 'D00F5D5217896FB7FD601412CB890830');
 
 -- --------------------------------------------------------
@@ -47,21 +58,32 @@ INSERT INTO `tbladmin` (`Id`, `firstName`, `lastName`, `emailAddress`, `password
 -- Table structure for table `tblattendance`
 --
 
-CREATE TABLE `tblattendance` (
-  `Id` int(10) NOT NULL,
-  `admissionNo` varchar(255) NOT NULL,
-  `classId` varchar(10) NOT NULL,
-  `classArmId` varchar(10) NOT NULL,
-  `sessionTermId` varchar(10) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `dateTimeTaken` varchar(20) NOT NULL
+CREATE TABLE `tblattendance`
+(
+  `Id` int
+(10) NOT NULL,
+  `admissionNo` varchar
+(255) NOT NULL,
+  `classId` varchar
+(10) NOT NULL,
+  `classArmId` varchar
+(10) NOT NULL,
+  `sessionTermId` varchar
+(10) NOT NULL,
+  `status` varchar
+(10) NOT NULL,
+  `dateTimeTaken` varchar
+(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblattendance`
 --
 
-INSERT INTO `tblattendance` (`Id`, `admissionNo`, `classId`, `classArmId`, `sessionTermId`, `status`, `dateTimeTaken`) VALUES
+INSERT INTO `tblattendance` (`
+Id`,
+`admissionNo
+`, `classId`, `classArmId`, `sessionTermId`, `status`, `dateTimeTaken`) VALUES
 (162, 'ASDFLKJ', '1', '2', '1', '1', '2020-11-01'),
 (163, 'HSKSDD', '1', '2', '1', '1', '2020-11-01'),
 (164, 'JSLDKJ', '1', '2', '1', '1', '2020-11-01'),
@@ -98,16 +120,22 @@ INSERT INTO `tblattendance` (`Id`, `admissionNo`, `classId`, `classArmId`, `sess
 -- Table structure for table `tblclass`
 --
 
-CREATE TABLE `tblclass` (
-  `Id` int(10) NOT NULL,
-  `className` varchar(255) NOT NULL
+CREATE TABLE `tblclass`
+(
+  `Id` int
+(10) NOT NULL,
+  `className` varchar
+(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblclass`
 --
 
-INSERT INTO `tblclass` (`Id`, `className`) VALUES
+INSERT INTO `tblclass` (`
+Id`,
+`className
+`) VALUES
 (1, 'Seven'),
 (3, 'Eight'),
 (4, 'Nine');
@@ -118,18 +146,26 @@ INSERT INTO `tblclass` (`Id`, `className`) VALUES
 -- Table structure for table `tblclassarms`
 --
 
-CREATE TABLE `tblclassarms` (
-  `Id` int(10) NOT NULL,
-  `classId` varchar(10) NOT NULL,
-  `classArmName` varchar(255) NOT NULL,
-  `isAssigned` varchar(10) NOT NULL
+CREATE TABLE `tblclassarms`
+(
+  `Id` int
+(10) NOT NULL,
+  `classId` varchar
+(10) NOT NULL,
+  `classArmName` varchar
+(255) NOT NULL,
+  `isAssigned` varchar
+(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblclassarms`
 --
 
-INSERT INTO `tblclassarms` (`Id`, `classId`, `classArmName`, `isAssigned`) VALUES
+INSERT INTO `tblclassarms` (`
+Id`,
+`classId
+`, `classArmName`, `isAssigned`) VALUES
 (2, '1', 'S1', '1'),
 (4, '1', 'S2', '1'),
 (5, '3', 'E1', '1'),
@@ -141,23 +177,36 @@ INSERT INTO `tblclassarms` (`Id`, `classId`, `classArmName`, `isAssigned`) VALUE
 -- Table structure for table `tblclassteacher`
 --
 
-CREATE TABLE `tblclassteacher` (
-  `Id` int(10) NOT NULL,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  `emailAddress` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phoneNo` varchar(50) NOT NULL,
-  `classId` varchar(10) NOT NULL,
-  `classArmId` varchar(10) NOT NULL,
-  `dateCreated` varchar(50) NOT NULL
+CREATE TABLE `tblclassteacher`
+(
+  `Id` int
+(10) NOT NULL,
+  `firstName` varchar
+(255) NOT NULL,
+  `lastName` varchar
+(255) NOT NULL,
+  `emailAddress` varchar
+(255) NOT NULL,
+  `password` varchar
+(255) NOT NULL,
+  `phoneNo` varchar
+(50) NOT NULL,
+  `classId` varchar
+(10) NOT NULL,
+  `classArmId` varchar
+(10) NOT NULL,
+  `dateCreated` varchar
+(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblclassteacher`
 --
 
-INSERT INTO `tblclassteacher` (`Id`, `firstName`, `lastName`, `emailAddress`, `password`, `phoneNo`, `classId`, `classArmId`, `dateCreated`) VALUES
+INSERT INTO `tblclassteacher` (`
+Id`,
+`firstName
+`, `lastName`, `emailAddress`, `password`, `phoneNo`, `classId`, `classArmId`, `dateCreated`) VALUES
 (1, 'Will', 'Williams', 'teacher@mail.com', '32250170a0dca92d53ec9624f336ca24', '09089898999', '1', '2', '2020-10-31'),
 (4, 'Demola', 'Ade', 'Kumolu@gmail.com', '32250170a0dca92d53ec9624f336ca24', '09672002882', '1', '4', '2020-11-01'),
 (5, 'Ryan', 'McQuie', 'ryan@mail.com', '32250170a0dca92d53ec9624f336ca24', '7014560000', '3', '5', '2021-10-07'),
@@ -169,19 +218,28 @@ INSERT INTO `tblclassteacher` (`Id`, `firstName`, `lastName`, `emailAddress`, `p
 -- Table structure for table `tblsessionterm`
 --
 
-CREATE TABLE `tblsessionterm` (
-  `Id` int(10) NOT NULL,
-  `sessionName` varchar(50) NOT NULL,
-  `termId` varchar(50) NOT NULL,
-  `isActive` varchar(10) NOT NULL,
-  `dateCreated` varchar(50) NOT NULL
+CREATE TABLE `tblsessionterm`
+(
+  `Id` int
+(10) NOT NULL,
+  `sessionName` varchar
+(50) NOT NULL,
+  `termId` varchar
+(50) NOT NULL,
+  `isActive` varchar
+(10) NOT NULL,
+  `dateCreated` varchar
+(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblsessionterm`
 --
 
-INSERT INTO `tblsessionterm` (`Id`, `sessionName`, `termId`, `isActive`, `dateCreated`) VALUES
+INSERT INTO `tblsessionterm` (`
+Id`,
+`sessionName
+`, `termId`, `isActive`, `dateCreated`) VALUES
 (1, '2019/2020', '1', '1', '2020-10-31'),
 (3, '2019/2020', '2', '0', '2020-10-31');
 
@@ -191,23 +249,36 @@ INSERT INTO `tblsessionterm` (`Id`, `sessionName`, `termId`, `isActive`, `dateCr
 -- Table structure for table `tblstudents`
 --
 
-CREATE TABLE `tblstudents` (
-  `Id` int(10) NOT NULL,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  `otherName` varchar(255) NOT NULL,
-  `admissionNumber` varchar(255) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `classId` varchar(10) NOT NULL,
-  `classArmId` varchar(10) NOT NULL,
-  `dateCreated` varchar(50) NOT NULL
+CREATE TABLE `tblstudents`
+(
+  `Id` int
+(10) NOT NULL,
+  `firstName` varchar
+(255) NOT NULL,
+  `lastName` varchar
+(255) NOT NULL,
+  `otherName` varchar
+(255) NOT NULL,
+  `admissionNumber` varchar
+(255) NOT NULL,
+  `password` varchar
+(50) NOT NULL,
+  `classId` varchar
+(10) NOT NULL,
+  `classArmId` varchar
+(10) NOT NULL,
+  `dateCreated` varchar
+(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblstudents`
 --
 
-INSERT INTO `tblstudents` (`Id`, `firstName`, `lastName`, `otherName`, `admissionNumber`, `password`, `classId`, `classArmId`, `dateCreated`) VALUES
+INSERT INTO `tblstudents` (`
+Id`,
+`firstName
+`, `lastName`, `otherName`, `admissionNumber`, `password`, `classId`, `classArmId`, `dateCreated`) VALUES
 (1, 'Thomas', 'Griswold', 'none', 'AMS005', '12345', '1', '2', '2020-10-31'),
 (3, 'Samuel', 'Rosella', 'none', 'AMS007', '12345', '1', '2', '2020-10-31'),
 (4, 'Milagros', 'Lawson', 'none', 'AMS011', '12345', '1', '2', '2020-10-31'),
@@ -231,16 +302,22 @@ INSERT INTO `tblstudents` (`Id`, `firstName`, `lastName`, `otherName`, `admissio
 -- Table structure for table `tblterm`
 --
 
-CREATE TABLE `tblterm` (
-  `Id` int(10) NOT NULL,
-  `termName` varchar(20) NOT NULL
+CREATE TABLE `tblterm`
+(
+  `Id` int
+(10) NOT NULL,
+  `termName` varchar
+(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblterm`
 --
 
-INSERT INTO `tblterm` (`Id`, `termName`) VALUES
+INSERT INTO `tblterm` (`
+Id`,
+`termName
+`) VALUES
 (1, 'First'),
 (2, 'Second'),
 (3, 'Third');
@@ -253,49 +330,57 @@ INSERT INTO `tblterm` (`Id`, `termName`) VALUES
 -- Indexes for table `tbladmin`
 --
 ALTER TABLE `tbladmin`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- Indexes for table `tblattendance`
 --
 ALTER TABLE `tblattendance`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- Indexes for table `tblclass`
 --
 ALTER TABLE `tblclass`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- Indexes for table `tblclassarms`
 --
 ALTER TABLE `tblclassarms`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- Indexes for table `tblclassteacher`
 --
 ALTER TABLE `tblclassteacher`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- Indexes for table `tblsessionterm`
 --
 ALTER TABLE `tblsessionterm`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- Indexes for table `tblstudents`
 --
 ALTER TABLE `tblstudents`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- Indexes for table `tblterm`
 --
 ALTER TABLE `tblterm`
-  ADD PRIMARY KEY (`Id`);
+ADD PRIMARY KEY
+(`Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -305,42 +390,50 @@ ALTER TABLE `tblterm`
 -- AUTO_INCREMENT for table `tbladmin`
 --
 ALTER TABLE `tbladmin`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tblattendance`
 --
 ALTER TABLE `tblattendance`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
 --
 -- AUTO_INCREMENT for table `tblclass`
 --
 ALTER TABLE `tblclass`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tblclassarms`
 --
 ALTER TABLE `tblclassarms`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `tblclassteacher`
 --
 ALTER TABLE `tblclassteacher`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `tblsessionterm`
 --
 ALTER TABLE `tblsessionterm`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tblstudents`
 --
 ALTER TABLE `tblstudents`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `tblterm`
 --
 ALTER TABLE `tblterm`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int
+(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
