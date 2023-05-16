e8a6ed83754e22342b10d8b7d59fef0ba7a7d423
Rensutsuk <johnrenzoerfelp@gmail.com>
2023-05-16 22:42:18 +0800

    added cert

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -1,4 +1,5 @@
 <?php
 $con = mysqli_init();
+mysqli_ssl_set($con, NULL, NULL, "Includes\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "attendace-db.mysql.database.azure.com", "JohnRenzo", "^9e71k2eGk1M%T4m", "attendace-db", 3306, MYSQLI_CLIENT_SSL);
 ?>
