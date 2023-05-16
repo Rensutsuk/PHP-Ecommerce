383e007a0ecaa4811d4d61d608b9bce02becfae0
Rensutsuk <johnrenzoerfelp@gmail.com>
2023-05-16 22:47:00 +0800

    changed dbcon

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -1,5 +1,12 @@
 <?php
-$con = mysqli_init();
+$host = "attendace-db.mysql.database.azure.com";
+$user = "JohnRenzo";
+$pass = "^9e71k2eGk1M%T4m";
+$db = "attendace-db";
+
+$conn = new mysqli($host, $user, $pass, $db);
 mysqli_ssl_set($con, NULL, NULL, "Includes\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "attendace-db.mysql.database.azure.com", "JohnRenzo", "^9e71k2eGk1M%T4m", "attendace-db", 3306, MYSQLI_CLIENT_SSL);
+if ($conn->connect_error) {
+    echo "Seems like you have not configured the database. Failed To Connect to database:" . $conn->connect_error;
+}
 ?>
