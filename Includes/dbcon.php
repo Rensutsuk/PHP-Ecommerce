c347a35896925f4d8eb7d9a326a48111907c830a
Rensutsuk <johnrenzoerfelp@gmail.com>
2023-05-16 22:10:04 +0800

    comited

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -1,15 +1,4 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:project-omegasql.database.windows.net,1433; Database = project-omegaDB", "Rensutsuki", "Gja1Chl9yptC1RB^");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Rensutsuki", "pwd" => "Gja1Chl9yptC1RB^", "Database" => "project-omegaDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:project-omegasql.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$con = mysqli_init();
+mysqli_real_connect($conn, "attendace-db.mysql.database.azure.com", "JohnRenzo", "^9e71k2eGk1M%T4m", "attendace-db", 3306, MYSQLI_CLIENT_SSL);
 ?>
